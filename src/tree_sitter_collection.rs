a54277590a36db2474673f62772537ecf495e120
benwis <ben@celcyon.com>
2024-04-11 00:32:58 -0700

    Upgraded some stuff, tweaked some things. Issue with dup() and multiple treesitter versions

--- src/tree_sitter_collection.rs
+++ src/tree_sitter_collection.rs
@@ -7,9 +7,11 @@ impl TreeSitterCollection {
     pub fn rust() -> TreeSitterCollection {
         let rust_conf = HighlightConfiguration::new(
             tree_sitter_rstml::language_rust_with_rstml(),
+            "rust",
             tree_sitter_rstml::HIGHLIGHTS_QUERY,
             "",
             "",
+            true,
         )
         .unwrap();
 
@@ -24,9 +26,11 @@ impl TreeSitterCollection {
 
         let conf = HighlightConfiguration::new(
             tree_sitter_typescript::language_typescript(),
+            "typescript",
             &highlights,
             tree_sitter_javascript::INJECTION_QUERY,
             &locals,
+            true,
         )
         .unwrap();
 
@@ -43,9 +47,11 @@ impl TreeSitterCollection {
 
         let conf = HighlightConfiguration::new(
             tree_sitter_typescript::language_tsx(),
+            "tsx",
             &highlights,
             tree_sitter_javascript::INJECTION_QUERY,
             &locals,
+            true,
         )
         .unwrap();
 
@@ -54,9 +60,11 @@ impl TreeSitterCollection {
     pub fn javascript() -> TreeSitterCollection {
         let conf = HighlightConfiguration::new(
             tree_sitter_javascript::language(),
+            "javascript",
             tree_sitter_javascript::HIGHLIGHT_QUERY,
             tree_sitter_javascript::INJECTION_QUERY,
             tree_sitter_javascript::LOCALS_QUERY,
+            true,
         )
         .unwrap();
 
@@ -65,9 +73,11 @@ impl TreeSitterCollection {
     pub fn jsx() -> TreeSitterCollection {
         let conf = HighlightConfiguration::new(
             tree_sitter_javascript::language(),
+            "jsx",
             tree_sitter_javascript::JSX_HIGHLIGHT_QUERY,
             tree_sitter_javascript::INJECTION_QUERY,
             "",
+            true,
         )
         .unwrap();
 
@@ -76,9 +86,11 @@ impl TreeSitterCollection {
     pub fn go() -> TreeSitterCollection {
         let conf = HighlightConfiguration::new(
             tree_sitter_go::language(),
+            "go",
             tree_sitter_go::HIGHLIGHT_QUERY,
             "",
             "",
+            true,
         )
         .unwrap();
 
@@ -87,9 +99,11 @@ impl TreeSitterCollection {
     pub fn c() -> TreeSitterCollection {
         let conf = HighlightConfiguration::new(
             tree_sitter_c::language(),
+            "c",
             tree_sitter_c::HIGHLIGHT_QUERY,
             "",
             "",
+            true,
         )
         .unwrap();
 
@@ -98,9 +112,11 @@ impl TreeSitterCollection {
     pub fn html() -> TreeSitterCollection {
         let conf = HighlightConfiguration::new(
             tree_sitter_html::language(),
+            "html",
             tree_sitter_html::HIGHLIGHT_QUERY,
             tree_sitter_html::INJECTION_QUERY,
             "",
+            true,
         )
         .unwrap();
 
@@ -109,9 +125,11 @@ impl TreeSitterCollection {
     pub fn toml() -> TreeSitterCollection {
         let conf = HighlightConfiguration::new(
             tree_sitter_toml::language(),
+            "toml",
             tree_sitter_toml::HIGHLIGHT_QUERY,
             "",
             "",
+            true,
         )
         .unwrap();
 
@@ -120,26 +138,37 @@ impl TreeSitterCollection {
     pub fn python() -> TreeSitterCollection {
         let conf = HighlightConfiguration::new(
             tree_sitter_python::language(),
+            "python",
             tree_sitter_python::HIGHLIGHT_QUERY,
             "",
             "",
+            true,
         )
         .unwrap();
 
         TreeSitterCollection { conf }
     }
     pub fn dockerfile() -> TreeSitterCollection {
-        let conf =
-            HighlightConfiguration::new(tree_sitter_dockerfile::language(), "", "", "").unwrap();
+        let conf = HighlightConfiguration::new(
+            tree_sitter_dockerfile::language(),
+            "dockerfile",
+            "",
+            "",
+            "",
+            true,
+        )
+        .unwrap();
 
         TreeSitterCollection { conf }
     }
     pub fn json() -> TreeSitterCollection {
         let conf = HighlightConfiguration::new(
             tree_sitter_json::language(),
+            "json",
             tree_sitter_json::HIGHLIGHT_QUERY,
             "",
             "",
+            true,
         )
         .unwrap();
 
@@ -154,9 +183,11 @@ impl TreeSitterCollection {
     pub fn nix() -> TreeSitterCollection {
         let conf = HighlightConfiguration::new(
             tree_sitter_nix::language(),
+            "nix",
             tree_sitter_nix::HIGHLIGHTS_QUERY,
             "",
             "",
+            true,
         )
         .unwrap();
         TreeSitterCollection { conf }
